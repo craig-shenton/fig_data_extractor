51dc1bc79d895e3ea58aadf301d177e8bceeac44
Craig Robert Shenton <c.shenton@surrey.ac.uk>
2016-10-15 23:00:31 +0100

    added script

---
+++ fig_data_extract.m
@@ -0,0 +1,16 @@
+%% Extract data from figure without accompning matlab code
+% https://uk.mathworks.com/matlabcentral/answers/100687#answer_110036
+
+open('...\example.fig');
+
+h = gcf;                              %current figure handle
+
+axesObjs = get(h, 'Children');        %axes handles
+dataObjs = get(axesObjs, 'Children'); %handles to low-level graphics objects in axes
+      
+objTypes = get(dataObjs, 'Type');     %type of low-level graphics object
+
+
+x = get(dataObjs, 'XData');           %data from low-level grahics objects
+y = get(dataObjs, 'YData');
+z = get(dataObjs, 'ZData');
